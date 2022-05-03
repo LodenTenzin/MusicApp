32a3976df5601443efd2e81ff46312a22462650f
LodenTenzin <tenzintibetloden@gmail.com>
2022-05-03 13:04:56 -0400

    Song player component with time update,drag func,pause play toggle

--- react-musicplayer/src/App.js
+++ react-musicplayer/src/App.js
@@ -1,7 +1,22 @@
+import React, {useState} from "react";
+//Import Styles
+import './styles/app.scss';
+//Adding Components
+import Player from './components/Player';
+import Song from './components/Songs';
+//Import Util
+import data from './util';
+
 function App() {
+  //State
+  const [song, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(song[4]);
+  const [isPlaying, setisPlaying] = useState(false);
+
   return (
     <div className="App">
-       <h1>Music Player</h1>
+       <Song currentSong={currentSong}/>
+       <Player isPlaying={isPlaying} setisPlaying={setisPlaying} currentSong={currentSong}/>
     </div>
   );
 }
