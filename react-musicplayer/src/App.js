1bb4514f05dc0a4e712a7f4f2bc03a39e804c752
LodenTenzin <tenzintibetloden@gmail.com>
2022-05-09 18:47:53 -0400

    Library UI and styling

--- react-musicplayer/src/App.js
+++ react-musicplayer/src/App.js
@@ -4,19 +4,21 @@ import './styles/app.scss';
 //Adding Components
 import Player from './components/Player';
 import Song from './components/Songs';
+import Library from "./components/Library";
 //Import Util
 import data from './util';
 
 function App() {
   //State
-  const [song, setSongs] = useState(data());
-  const [currentSong, setCurrentSong] = useState(song[4]);
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[4]);
   const [isPlaying, setisPlaying] = useState(false);
 
   return (
     <div className="App">
        <Song currentSong={currentSong}/>
        <Player isPlaying={isPlaying} setisPlaying={setisPlaying} currentSong={currentSong}/>
+       <Library songs={songs}/>
     </div>
   );
 }
